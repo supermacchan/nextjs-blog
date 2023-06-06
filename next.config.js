0eed7535d54e28befefed368db61381f73692f12
Olga Birkovska <aiba.aki9@gmail.com>
2023-06-06 20:49:43 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -28,7 +28,7 @@ module.exports = {
     loader: 'imgix',
     // path: 'supermacchan-blog',
     path: 'https://supermacchan-blog.imgix.net',
-    formats: ['image/webp', 'image/jpeg', 'image/png', 'image/jpg'],
+    formats: ['image/webp'],
     // disableStaticImages: false,
     // remotePatterns: [
     //   {
