7816587e38c7eb5b143d94596a3215956008c60f
Olga Birkovska <aiba.aki9@gmail.com>
2023-06-06 23:20:24 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,18 +16,27 @@ module.exports = {
   images: {
     loader: 'imgix',
     path: 'https://supermacchan-blog.imgix.net',
-  },
-  async headers() {
-    return [
+    domains: ["https://supermacchan-blog.imgix.net", "https://supermacchan.github.io/nextjs-blog/"],
+    remotePatterns: [
       {
-        source: "//",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Content-Type", value: "image/webp"}
-        ],
-      },
-    ]
+        protocol: "https",
+        hostname: "**/imgix.net",
+        port: '',
+        pathname: "/images/**"
+      }
+  ]
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "//",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Origin", value: "*" },
+  //         { key: "Content-Type", value: "image/webp"}
+  //       ],
+  //     },
+  //   ]
+  // },
 }
 
   
