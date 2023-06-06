2c5f147323e363b28b6605ef4ffe5892449d65f4
Olga Birkovska <aiba.aki9@gmail.com>
2023-06-06 22:52:17 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,19 +1,9 @@
-// const repo = 'nextjs-blog'
-// const assetPrefix = `/${repo}/`
-// const basePath = `/${repo}`
-
-// module.exports = {
-//   assetPrefix: assetPrefix,
-//   basePath: basePath,
-// }
-
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = ''
-let basePath = '/'
+let basePath = ''
 
 if (isGithubActions) {
-  // trim off `<owner>/`
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
 
   assetPrefix = `/${repo}/`
@@ -23,28 +13,20 @@ if (isGithubActions) {
 module.exports = {
   assetPrefix: assetPrefix,
   basePath: basePath,
-//   === so that the images could be uploadable to gh-pages ===
   images: {
     loader: 'imgix',
-    // path: 'supermacchan-blog',
     path: 'https://supermacchan-blog.imgix.net',
-    // formats: ['image/webp'],
-    // disableStaticImages: false,
-    // remotePatterns: [
-    //   {
-    //     protocol: 'https',
-    //     hostname: 'https://supermacchan-blog.imgix.net',
-    //   },
-    // ],
   },
-  // async headers() {
-  //   return [
-  //     {
-  //       source: "//supermacchan.github.io/nextjs-blog/",
-  //       headers: [
-  //         { key: "Access-Control-Allow-Origin", value: "*" },
-  //       ],
-  //     },
-  //   ]
-  // },
+  async headers() {
+    return [
+      {
+        source: "//",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+        ],
+      },
+    ]
+  },
 }
+
+  
